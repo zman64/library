88da44078d907232f2223d5537868c4621eb1f9c
Zachary Kahlig <zkahlig64@gmail.com>
2023-06-12 10:58:45 -0400

    Add book cards and book card structure

--- app.js
+++ app.js
@@ -1,9 +1,98 @@
-let myLibrary = [];
+const cardwrapper = document.querySelector(".card-wrapper")
 
-function Book() {
+let book = {
+    title: "Mystery",
+    author: "ned stranger",
+    pages: 445,
+    isCompleted: false,
+}
+
+let myLibrary = [
+    {
+        title: "Lord of the Rings",
+        author: "JRR Tolkien",
+        pages: 345,
+        isCompleted: true,
+    }
+];
+
+function createBookCard() {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("card");
+    return bookCard;
+}
+
+function populateBookCard(bookCard, book) {
+
+    const author = document.createElement('div');
+    author.classList.add('author');
+    author.textContent = book.author;
+    bookCard.appendChild(author);
+
+    const title = document.createElement('div');
+    title.classList.add('title');
+    title.textContent = book.title;
+    bookCard.appendChild(title);
+
+    const pages = document.createElement('div');
+    pages.classList.add('pages');
+    pages.textContent = book.pages;
+    bookCard.appendChild(pages);
+
+    const isCompleted = document.createElement('div');
+    isCompleted.classList.add('read');
+    isCompleted.textContent = book.isCompleted ? 'Has read' : 'Not read yet';
+    bookCard.appendChild(isCompleted);
 
 }
 
-function addBookToLibrary() {
+function appendBookCardToWrapper(bookCard) {
+    cardwrapper.appendChild(bookCard);
+}
+
+function Book(title, author, pages, isCompleted) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isCompleted = isCompleted;
+
+}
+
+function addBookToLibrary(title, author, pages, isCompleted) {
     // do stuff here
+    const newBook = new Book(title, author, pages, isCompleted);
+    myLibrary.push(book)
 }
+
+function displayBooks(library) {
+    library.forEach(book => {
+        const bookCard = createBookCard();
+        populateBookCard(bookCard, book);
+        appendBookCardToWrapper(bookCard);
+    });
+}
+
+// Function to handle form submission
+document.getElementById("book-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const isCompleted = document.getElementById("isCompleted").checked;
+
+    addBookToLibrary(title, author, pages, isCompleted);
+
+    // clear the input fields after submission
+    document.getElementById("title").value = '';
+    document.getElementById("author").value = '';
+    document.getElementById("pages").value = '';
+    document.getElementById("isCompleted").checked = false;
+
+    // Re-render the book list to include the new book
+    cardwrapper.textContent = ""; // Clear existing book cards
+    displayBooks(myLibrary);
+})
+
+addBookToLibrary(book);
+displayBooks(myLibrary);
